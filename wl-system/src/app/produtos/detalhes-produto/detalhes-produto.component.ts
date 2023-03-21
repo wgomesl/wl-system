1526c9075b55bac14f5aa37df32cec9b2db3edcd
wesley <wesley.gomes222@hotmail.com>
2023-03-20 22:03:12 -0300

    adicionando o carrinho

--- wl-system/src/app/produtos/detalhes-produto/detalhes-produto.component.ts
+++ wl-system/src/app/produtos/detalhes-produto/detalhes-produto.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { IProduto } from 'src/app/produtos';
 import { ProdutosService } from 'src/app/produtos.service';
+import { NotificacaoService } from 'src/app/notificacao.service';
 
 @Component({
   selector: 'app-detalhes-produto',
@@ -14,12 +15,17 @@ export class DetalhesProdutoComponent implements OnInit{
 
   constructor(
     private produtosService: ProdutosService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private notificacaoService: NotificacaoService
   ){}
 
   ngOnInit(): void {
       const routeParams = this.route.snapshot.paramMap;
       const produtoId = Number(routeParams.get("id"));
       this.produto = this.produtosService.getOne(produtoId);
+  }
+
+  adicionarAoCarrinho(){
+    this.notificacaoService.notificar("O produto foi adicionado ao carrinho");
   }
 }
